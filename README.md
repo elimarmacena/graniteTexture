5a28952cbd8d7965a2125d2c0608446e3522b8c5
elimarmacena <elimar.macena@gmail.com>
2019-11-10 15:47:23 -0300

    UPDATE: now opening a image we convert to greyscale and then made a matrix

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # graniteTexture
 repository used to develop my final  university graduate work
 
-### Library
+### Librarys
 The work use the following library:
 * PIL
 * numpy
--- imgMatrix.py
+++ imgMatrix.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 def imgToMatrix(fileLocation:str):
-    image = Image.open(fileLocation)
+    #open the passed image as a gray scale image
+    image = Image.open(fileLocation).convert('LA')
     img_ar = np.asarray(image)
     return(img_ar)
