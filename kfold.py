eaffe2e91545414fd5e18846c95842fd24f55e18
elimarmacena <elimar.macena@gmail.com>
2020-03-05 20:34:37 -0300

    ADD: k fold implementation

---
+++ kfold.py
@@ -0,0 +1,14 @@
+from random import seed
+from random import randrange #default python random library
+def kfold(data_sample:list, fold_num = 4, seed_value = 1):
+    seed(seed_value)
+    final_dataset = []
+    data_keeper = data_sample.copy()
+    fold_size = len(data_sample) // fold_num #if the number of records isn't divisible for the number of folds we lost records.
+    for i in range(fold_num):
+        fold_data = []
+        for i in range(fold_size):
+            index = randrange(0,len(data_keeper))
+            fold_data.append(data_keeper.pop(index))
+        final_dataset.append(fold_data)
+    return final_dataset
