e4185381a4aa0ca3a8ec102e96c1251ec24d5406
elimarmacena <elimar.macena@gmail.com>
2020-03-11 22:40:22 -0300

    UPDATE: comments remove

--- main.py
+++ main.py
@@ -6,31 +6,23 @@ import lbp
 import cmct
 # TODO save each algorith information in a CSV file
 def main():
-	img_path = commons.getAllImages()[0]
-	#summary_path = commons.summaryzeImgPaths()
+	summary_path = commons.summaryzeImgPaths()
 	lbp_file = commons.writeFile('lbp_results.csv')
 	cmct_file = commons.writeFile('cmct_results.csv')
 	ecmct_file = commons.writeFile('ecmct_result.csv')
-
-	lbp_histogram = lbp.lbp(img_path)
-	commons.addFileLine(''+img_path + ',' + (','.join(str(index_value) for index_value in lbp_histogram )) + '\n', lbp_file)
-
-	cmct_histogram = cmct.cmct(img_path)
-	commons.addFileLine(''+img_path + ',' + (','.join(str(index_value) for index_value in cmct_histogram )) + '\n', cmct_file)
-
-	ecmct_histogram = ecmct.ecmct(img_path)
-	commons.addFileLine(''+img_path + ',' + (','.join(str(index_value) for index_value in ecmct_histogram )) + '\n', ecmct_file)
-	# for granite_type  in summary_path.keys():
-	# 	for img_path in summary_path[granite_type]:
-	# 		# LBP SECTION
-	# 		lbp_histogram = lbp.lbp(img_path)
-	# 		commons.addFileLine(''+img_path + ',' + (','.join(str(index_value) for index_value in lbp_histogram )) + '\n', lbp_file)
-	# 		#CMCT SECTION
-	# 		cmct_histogram = cmct.cmct(img_path)
-	# 		commons.addFileLine(''+img_path + ',' + (','.join(str(index_value) for index_value in cmct_histogram )) + '\n', cmct_file)
-	# 		#ECMCT SECTION
-	# 		ecmct_histogram = ecmct.ecmct(img_path)
-	# 		commons.addFileLine(''+img_path + ',' + (','.join(str(index_value) for index_value in ecmct_histogram )) + '\n', ecmct_file)
+	
+	for granite_type  in summary_path.keys():
+		for img_path in summary_path[granite_type]:
+			# LBP SECTION
+			lbp_histogram = lbp.lbp(img_path)
+			commons.addFileLine(''+img_path + ',' + (','.join(str(index_value) for index_value in lbp_histogram )) + '\n', lbp_file)
+			#CMCT SECTION
+			cmct_histogram = cmct.cmct(img_path)
+			commons.addFileLine(''+img_path + ',' + (','.join(str(index_value) for index_value in cmct_histogram )) + '\n', cmct_file)
+			#ECMCT SECTION
+			ecmct_histogram = ecmct.ecmct(img_path)
+			commons.addFileLine(''+img_path + ',' + (','.join(str(index_value) for index_value in ecmct_histogram )) + '\n', ecmct_file)
+	
 	commons.closeFile(lbp_file)
 	commons.closeFile(cmct_file)
 	commons.closeFile(ecmct_file)
