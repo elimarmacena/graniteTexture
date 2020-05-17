fe95100c63a81a93fd9a8a847c8dcee73bd55f99
elimarmacena <elimar.macena@gmail.com>
2020-05-17 19:39:08 -0300

    FIX: lbp window navegation

--- lbp.py
+++ lbp.py
@@ -8,39 +8,33 @@ def lbp(file_locate:str):
         raise Exception('Matrix size is not valid')
     row_agent = 0
     lbp_histogram = Commons.startHistogram(256)
-    while(row_agent <= (num_row - 3)):
+    while(row_agent < num_row):
         column_agent = 0
-        while(column_agent <= (num_col - 3)):
-            #sub image with a size equals 3x3
-            macro_img = matrix[row_agent:(row_agent+3),column_agent:(column_agent+3)]
-            lbp_value = _findLbpMacro(macro_img)
+        while(column_agent <num_col):
+            # top_left, top_middle, top_right, 
+            macro_data = [get_neighbor(row_agent-1,column_agent-1,matrix),get_neighbor(row_agent-1, column_agent,matrix),get_neighbor(row_agent-1,column_agent+1,matrix),
+            # middle_left, middle_right,
+            get_neighbor(row_agent,column_agent-1,matrix),get_neighbor(row_agent, column_agent+1,matrix),
+            # bot_left, bot_middle, bot_right, 
+            get_neighbor(row_agent+1,column_agent-1,matrix),get_neighbor(row_agent+1, column_agent,matrix),get_neighbor(row_agent+1,column_agent+1,matrix)]
+            middle_pixel = get_neighbor(row_agent,column_agent,matrix)
+            
+            lbp_value = bit_value(macro_data,middle_pixel)
             lbp_histogram[lbp_value] += 1
             column_agent += 1
         row_agent +=1
     return lbp_histogram
             
-def _findLbpMacro(matrix:np.array):
-    middle          = matrix[1][1]
-    #matrix first line
-    top_left        = matrix[0][0]
-    top_middle      = matrix[0][1]
-    top_right       = matrix[0][2]
-    
-    #matrix second line
-    middle_left     = matrix[1][0]
-    middle_right    = matrix[1][2]
-    
-    #matrix final line
-    bottom_left     = matrix[2][0]
-    bottom_middle   = matrix[2][1]
-    bottom_right     = matrix[2][2]
-
-    lbp_order = [top_left,middle_left,bottom_left,bottom_middle,bottom_right,middle_right,top_right,top_middle]
-    value = _macroValue(lbp_order, middle)
-    return value
-
-def _macroValue(neighborhood:list, center_value:int):
+def bit_value(neighborhood:list, center_value:int):
+    counter_clockwise = [neighborhood[0],neighborhood[3],neighborhood[5],neighborhood[6],neighborhood[7],neighborhood[4],neighborhood[2],neighborhood[1]]
     bit_string = ''
-    for neighbor_value in neighborhood:
+    for neighbor_value in counter_clockwise:
         bit_string = bit_string + ('1' if (neighbor_value >= center_value) else '0')
     return int(bit_string,2)
+
+def get_neighbor(row_index:int,column_index:int,matrix:np.array):
+    if(row_index >= 0 and column_index >= 0):
+        return matrix[row_index][column_index]
+    else:
+        return 0
+    
